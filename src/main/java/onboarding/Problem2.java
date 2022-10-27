4ca72e125b51b838dc05d81eb2244bf2c63438fc
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 12:41:47 +0900

    Prob_2 feat : Check_same_char 메서드 추가
    전/후 Char와 비교하여 다른 글자만 answer행렬에 추가, answer를 return

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,6 +1,20 @@
 package onboarding;
 
 public class Problem2 {
+    public static String Check_same_char(String cryptogram)
+    {
+        char prev = '\0';
+        String answer = "";
+        int i = 0;
+        for (i = 0; i + 1 < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) != cryptogram.charAt(i + 1) && cryptogram.charAt(i) != prev)// 앞뒤 글자와 달라야함
+                answer = answer + cryptogram.charAt(i);
+            prev = cryptogram.charAt(i);
+        }
+        if (cryptogram.charAt(i) != prev)
+            answer = answer + cryptogram.charAt(i);
+        return answer;
+    }
     public static String solution(String cryptogram) {
         String answer = "";
 
