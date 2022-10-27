83d382b8e510caed8d3588bf26ec8a4eedd70eab
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 17:55:10 +0900

    Prob_7 Style : 변수 이름 변경
    for에서 for each로 변경, 변수 및 인덱스 명 일부 변경

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -6,40 +6,46 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Problem7 {
+    private static List<String> find_user_friend(String user, List<List<String>> friends) {
+        List<String> user_friend = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++){
+            if (friends.get(i).get(0) == user){
+                user_friend.add(friends.get(i).get(1));
+            }
+            if (friends.get(i).get(1) == user) {
+                user_friend.add(friends.get(i).get(0));
+            }
+        }
+        return (user_friend);
+    }
+    private static void update_values(HashMap<String, Integer> friend_score, String friends, int point) {
+        int value;
+        if (friend_score.containsKey(friends)) {
+            value = friend_score.get(friends);
+            friend_score.put(friends, value + point);
+        }
+        else
+            friend_score.put(friends, point);
+    }
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = new ArrayList<>();
-        List<String> user_friend = new ArrayList<>();
+        List<String> user_friends = new ArrayList<>();
         HashMap<String, Integer> friend_score = new HashMap<String, Integer>();
-        int value = 0;
-        user_friend = find_user_friend(user, friends);
+        user_friends = find_user_friend(user, friends);
 
-        for (int i = 0; i < friends.size(); i++){
-            for (int j = 0; j < user_friend.size(); j++){
-                if (friends.get(i).get(0) == user_friend.get(j) && friends.get(i).get(1) != user){
-                    if (friend_score.containsKey(friends.get(i).get(1))) {
-                        value = friend_score.get(friends.get(i).get(1));
-                        friend_score.put(friends.get(i).get(1), value + 10);
-                    }
-                    else
-                        friend_score.put(friends.get(i).get(1), 10);
+        for (List<String> relation : friends){
+            for (String user_friend : user_friends){
+                if (relation.get(0) == user_friend && relation.get(1) != user){
+                    update_values(friend_score, relation.get(1), 10);
                 }
-                if (friends.get(i).get(1) == user_friend.get(j) && friends.get(i).get(0) != user){
-                    if (friend_score.containsKey(friends.get(i).get(0))) {
-                        value = friend_score.get(friends.get(i).get(0));
-                        friend_score.put(friends.get(i).get(0), value + 10);
-                    }
-                    else
-                        friend_score.put(friends.get(i).get(0), 10);
+                if (relation.get(1) == user_friend && relation.get(0) != user){
+                    update_values(friend_score, relation.get(0), 10);
                 }
             }
         }
         for (String visitor : visitors){
-            if (!user_friend.contains(visitor)) {
-                if (friend_score.containsKey(visitor)) {
-                    value = friend_score.get(visitor);
-                    friend_score.put(visitor, value + 1);
-                } else
-                    friend_score.put(visitor, 1);
+            if (!user_friends.contains(visitor)) {
+                update_values(friend_score, visitor, 1);
             }
         }
 
@@ -54,16 +60,5 @@ public class Problem7 {
         return answer;
     }
 
-    private static List<String> find_user_friend(String user, List<List<String>> friends) {
-        List<String> user_friend = new ArrayList<>();
-        for (int i = 0; i < friends.size(); i++){
-            if (friends.get(i).get(0) == user){
-                user_friend.add(friends.get(i).get(1));
-            }
-            if (friends.get(i).get(1) == user) {
-                user_friend.add(friends.get(i).get(0));
-            }
-        }
-        return (user_friend);
-    }
+
 }
