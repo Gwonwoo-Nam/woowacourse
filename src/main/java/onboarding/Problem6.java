724bd4fd948238196ce1b24697340e8a549398c6
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 13:23:07 +0900

    Prob_6 feat : main 메서드 추가
    배열 answer_rep를 선언하고, 두 글자를 substring으로 읽어 다른 문자열에 있으면 answer_rep에 저장, 중복 제거하여 answer를 return한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,31 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
+        List<String> answer = new ArrayList<>();
+        List<String> answer_rep = new ArrayList<>();
+        String temp = "";
+        for (int i = 0; i < forms.size(); i++){
+            for (int j = 0; j + 1< forms.get(i).get(1).length(); j++){
+                temp = forms.get(i).get(1).substring(j,j+2);
+                for (int k = i + 1; k < forms.size(); k++){
+                    if (forms.get(k).get(1).contains(temp)) {
+                        temp = forms.get(k).get(0);
+                        answer_rep.add(0,forms.get(k).get(0));
+                        answer_rep.add(0,forms.get(i).get(0));
+                    }
+                }
+            }
+        }
+        for (String email : answer_rep){
+            if(!answer.contains(email))
+                answer.add(email);
+        }
+        Collections.sort(answer);
         return answer;
     }
 }
