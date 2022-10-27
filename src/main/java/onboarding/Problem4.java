9474777cf5d958b2345664f85907e5f20939b2f8
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 13:01:47 +0900

    Prob_4 feat : main 메서드 추가
    word의 문자를 읽어 소문자 reverse, 대문자 reverse를 실행, 그 외 그대로 반환한다.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,19 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        int diff = 0;
+        for (int i = 0; i < word.length(); i++){
+            if (word.charAt(i) >= 65 && word.charAt(i) <= 90) {
+                diff = word.charAt(i) - 65;
+                answer = answer + (char)(90 - diff);
+            }
+            else if (word.charAt(i) >= 97 && word.charAt(i) <= 122) {
+                diff = word.charAt(i) - 97;
+                answer = answer + (char)(122 - diff);
+            }
+            else
+                answer = answer + word.charAt(i);
+        }
         return answer;
     }
 }
