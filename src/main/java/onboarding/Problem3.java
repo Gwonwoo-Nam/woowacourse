40de56c89f62f7c4c9cceb76c20792a3b3bdb66b
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 12:57:44 +0900

    Prob_3 feat : 메인 메서드 추가
    number_digits 메서드를 호출하여 특정 숫자의 박수 개수를 count

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,11 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for (int i = 1; i <= number; i++)
+        {
+            answer += number_digits(i);
+        }
         return answer;
     }
 }
