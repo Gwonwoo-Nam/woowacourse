c999f01597dd3c3f0437fe38c318bc1a49b54c51
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 12:38:57 +0900

    Prob_2 feat : main function 추가
    Check_same_char를 호출하여 인접한 중복문자가 제거된 문자열을 cryptogram에 다시 저장, 문자열의 길이가 바뀌지 않을 때까지 반복.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,14 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = "";
+
+        while (cryptogram.length() != Check_same_char(cryptogram).length()) {
+            cryptogram = Check_same_char(cryptogram);
+            if (cryptogram.isEmpty())
+                break ;
+        }
+        answer = cryptogram;
         return answer;
     }
 }
