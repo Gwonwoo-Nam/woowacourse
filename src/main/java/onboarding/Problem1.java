4d687bfa027a2cf0bafa7b4e7850fbfc6e149acf
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 11:59:59 +0900

    feat : check_max, page_digits 기능 추가
    pobi와 crong의 페이지 계산 값을 반환하는 check_max 함수 구현. check_max는 page_digits을 호출하여 두 페이지 중 최대 결과 값을 반환함. page_digits은 Array에 각 자리를 저장하고 더한 값과 곱한 값을 비교함.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,8 +1,32 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Problem1 {
+    public static int page_digits(int digit) {
+        List<Integer> digits = new ArrayList<Integer>();
+        int sum_digits = 0;
+        int multiply_digits = 1;
+        int i = 0;
+
+        while (digit > 0) {
+            digits.add(digit % 10); // 각 자리를 digits 배열에 저장
+            digit = digit / 10;
+            i++;
+        }
+        for (int k = 0; k < i; k++) {
+            sum_digits += digits.get(k);
+            multiply_digits *= digits.get(k);
+        }
+        return (sum_digits >= multiply_digits ? sum_digits : multiply_digits);
+    }
+    public static int check_max(List<Integer> p) {
+        if (page_digits(p.get(0)) > page_digits(p.get(1)))
+            return page_digits(p.get(0));
+        else
+            return page_digits(p.get(1));
+    }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         int pobi_result;
