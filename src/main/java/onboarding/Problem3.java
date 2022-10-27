dc8bbecd6ff4cd3123c8b54903b7fd7341a9ff31
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 12:59:22 +0900

    Prob_3 feat : number_digits 메서드 추가
    각 자리를 digits 배열에 저장하여 3,6,9의 개수를 count하여 return한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,6 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
+    public static int number_digits(int number){
+        List<Integer> digits = new ArrayList<Integer>();
+        int i = 0;
+        int clap = 0;
+
+        while (number > 0) {
+            digits.add(number % 10); // 각 자리를 digits 배열에 저장
+            number = number / 10;
+            i++;
+        }
+        for (int j=0; j<digits.size(); j++){
+            if (digits.get(j) % 3 == 0 && digits.get(j) != 0)
+                clap++;
+        }
+        return clap;
+    }
     public static int solution(int number) {
         int answer = 0;
 
