3b7337c300e07cfbffdfcbdec0efd3298a34f486
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 13:50:36 +0900

    Prob_1 refactoring : save_digits
    기존 함수 page_digits - digits을 저장하는 메서드(save_digits)와 최대값을 계산하여 반환하는 메서드(calculate_max)로 나누어 refactoring

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -15,28 +15,34 @@ class Problem1 {
             return (-1);
         return (0);
     }
-    public static int page_digits(int digit) {
+    public static int calculate_max(int digit) {
         List<Integer> digits = new ArrayList<>();
         int sum_digits = 0;
         int multiply_digits = 1;
         int i = 0;
 
-        while (digit > 0) {
-            digits.add(digit % 10); // 각 자리를 digits 배열에 저장
-            digit = digit / 10;
-            i++;
-        }
+        i = save_digits(digit, digits, i);
         for (int k = 0; k < i; k++) {
             sum_digits += digits.get(k);
             multiply_digits *= digits.get(k);
         }
         return (sum_digits >= multiply_digits ? sum_digits : multiply_digits);
     }
+
+    private static int save_digits(int digit, List<Integer> digits, int i) {
+        while (digit > 0) {
+            digits.add(digit % 10); // 각 자리를 digits 배열에 저장
+            digit = digit / 10;
+            i++;
+        }
+        return i;
+    }
+
     public static int check_max(List<Integer> p) {
-        if (page_digits(p.get(0)) > page_digits(p.get(1)))
-            return page_digits(p.get(0));
+        if (calculate_max(p.get(0)) > calculate_max(p.get(1)))
+            return calculate_max(p.get(0));
         else
-            return page_digits(p.get(1));
+            return calculate_max(p.get(1));
     }
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
