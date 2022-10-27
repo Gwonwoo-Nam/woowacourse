ba250f0a3d2ed0910a3c76c889c6f5c96776fec1
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 14:22:16 +0900

    Prob_3 Refactor : getClap
    주어진 int를 arg로 받아 clap의 수를 리턴하는 refactor getclap

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem3 {
-    public static int number_digits(int number){
+    public static int num_to_clap(int number){
         List<Integer> digits = new ArrayList<Integer>();
         int i = 0;
         int clap = 0;
@@ -14,20 +14,25 @@ public class Problem3 {
             number = number / 10;
             i++;
         }
-        for (int j=0; j<digits.size(); j++){
+        clap = getClap(digits, clap);
+        return clap;
+    }
+
+    private static int getClap(List<Integer> digits, int clap) {
+        for (int j = 0; j< digits.size(); j++){
             if (digits.get(j) % 3 == 0 && digits.get(j) != 0)
                 clap++;
         }
         return clap;
     }
+
     public static int solution(int number) {
         int answer = 0;
+
         if (number > 10000 || number < 1)
             return (0);
         for (int i = 1; i <= number; i++)
-        {
-            answer += number_digits(i);
-        }
+            answer += num_to_clap(i);
         return answer;
     }
 }
