1585ca9e3bbce759bf243bbec735292b30161d26
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 17:19:59 +0900

    Prob_6 Refactor : find_answer
    find_answer / get_two_letters / add_repetition_to_answer로 3개로 refactor하였으며, 기존 메서드 이름을 가독성 개선 목적으로 변경, 변수 이름도 일부 변경 진행

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -4,42 +4,46 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+
 public class Problem6 {
-    private static int check_domain(String email){
+    private static boolean check_domain(String email){
         if (!email.endsWith("@email.com")) //예외처리 0 : 이메일 형식에 부합하는지
-            return (-1);
+            return false;
         if (email.length() < 11 || email.length() >= 20) // 예외처리 1 : email 길이 11~19
-            return (-1);
-        return (0);
+            return false;
+        return true;
     }
-    private static int check_nickname(String nickname){
+    private static boolean check_nickname(String nickname){
         if (!nickname.matches("^[ㄱ-ㅎ |ㅏ-ㅣ | 가-힣]*$")) // 예외처리 2 : nickname은 한글로만 구성됨
-            return (-1);
+            return false;
         if (nickname.length() < 1 || nickname.length() >= 20) // 예외처리 3 : nickname 길이 1~19
-            return (-1);
-        return (0);
+            return false;
+        return true;
     }
-    private static int check_email(List<List<String>> forms){
+    private static boolean check_counts(int count) {
+        if (count < 1 || count > 10000)// 예외처리 4 : 멤버의 number가 1~10000
+            return false;
+        return true;
+    }
+    private static boolean check_exceptions(List<List<String>> forms){
         int count = 0;
         for(List<String> items : forms) {
             count++;
-            if (check_domain(items.get(0)) == -1 || check_nickname(items.get(1)) == -1)
-                return (-1);
+            if (!check_domain(items.get(0)) || !check_nickname(items.get(1)))
+                return false;
         }
-        if (count < 1 || count > 10000)
-            return (-1); // 예외처리 4 : 멤버의 number가 1~10000
-        return (0);
+        if (!check_counts(count))
+            return false;
+        return true;
     }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
-        List<String> answer_rep = new ArrayList<>();
-        String temp = "";
-        if (check_email(forms) == -1)
+        List<String> answer_with_repetition = new ArrayList<>();
+
+        if (!check_exceptions(forms))
             return (List.of(""));
-        for (int i = 0; i < forms.size(); i++){
-            get_two_letters(forms, answer_rep, i);
-        }
-        for (String email : answer_rep){
+        answer_with_repetition = find_answer(forms);
+        for (String email : answer_with_repetition){
             if(!answer.contains(email))
                 answer.add(email);
         }
@@ -47,19 +51,27 @@ public class Problem6 {
         return answer;
     }
 
-    private static void get_two_letters(List<List<String>> forms, List<String> answer_rep, int i) {
-        String temp;
-        for (int j = 0; j + 1< forms.get(i).get(1).length(); j++){
-            temp = forms.get(i).get(1).substring(j,j+2);
-            add_answer(forms, answer_rep, temp, i);
+    private static List<String> find_answer(List<List<String>> forms) {
+        List<String> answer_with_repetition = new ArrayList<>();
+        for (int form_number = 0; form_number < forms.size(); form_number++){
+            get_two_letters(forms, answer_with_repetition, form_number);
+        }
+        return answer_with_repetition;
+    }
+
+    private static void get_two_letters(List<List<String>> forms, List<String> answer_with_repetition, int form_number) {
+        String two_letters;
+        for (int str_loc = 0; str_loc + 1< forms.get(form_number).get(1).length(); str_loc++){
+            two_letters = forms.get(form_number).get(1).substring(str_loc,str_loc+2);
+            add_repetition_to_answer(forms, answer_with_repetition, two_letters, form_number);
         }
     }
 
-    private static void add_answer(List<List<String>> forms, List<String> answer_rep, String temp, int i) {
-        for (int k = i + 1; k < forms.size(); k++){
-            if (forms.get(k).get(1).contains(temp)) {
-                answer_rep.add(forms.get(k).get(0));
-                answer_rep.add(forms.get(i).get(0));
+    private static void add_repetition_to_answer(List<List<String>> forms, List<String> answer_with_repetition, String temp, int form_number) {
+        for (int adjacent_form_number = form_number + 1; adjacent_form_number < forms.size(); adjacent_form_number++){
+            if (forms.get(adjacent_form_number).get(1).contains(temp)) {
+                answer_with_repetition.add(forms.get(adjacent_form_number).get(0));
+                answer_with_repetition.add(forms.get(form_number).get(0));
             }
         }
     }
