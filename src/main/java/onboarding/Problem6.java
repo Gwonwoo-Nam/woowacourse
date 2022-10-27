bf6beef932dd15685c806c77107e470ed3c0a2af
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 15:12:51 +0900

    Prob_6 feat : check_email 메서드 추가
    예외처리 : 멤버의 number가 1~10000개 범위인지 검사하며 이메일 도메인 양식에 맞춰져있는지 확인한다.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,18 +5,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
+
+    private static int check_email(List<List<String>> forms){
+        int count = 0;
+        for(List<String> items : forms) {
+            count++;
+            if (check_domain(items.get(0)) == -1)
+                return (-1);
+        }
+        if (count < 1 || count > 10000)
+            return (-1); // 예외처리 1 : 멤버의 number가 1~10000
+        return (0);
+    }
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = new ArrayList<>();
         List<String> answer_rep = new ArrayList<>();
         String temp = "";
+        if (check_email(forms) == -1)
+            return ();
         for (int i = 0; i < forms.size(); i++){
             for (int j = 0; j + 1< forms.get(i).get(1).length(); j++){
                 temp = forms.get(i).get(1).substring(j,j+2);
                 for (int k = i + 1; k < forms.size(); k++){
                     if (forms.get(k).get(1).contains(temp)) {
                         temp = forms.get(k).get(0);
-                        answer_rep.add(0,forms.get(k).get(0));
-                        answer_rep.add(0,forms.get(i).get(0));
+                        answer_rep.add(forms.get(k).get(0));
+                        answer_rep.add(forms.get(i).get(0));
                     }
                 }
             }
