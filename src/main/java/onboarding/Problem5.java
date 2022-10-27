fc7791a0d23d8725581b2f9c0c1fd08c4a89c2c7
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 14:46:51 +0900

    Prob_5 feat : 예외처리 추가
    예외처리 : money의 범위가 1~1000000이 아닐 때 빈 list를 리턴

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import org.mockito.internal.matchers.Null;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -8,6 +10,8 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         List<Integer> unit = new ArrayList<Integer>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
+        if (money < 1 || money > 1000000)
+            return (Collections.emptyList());
         for(int i = 0; i < 9; i++){
             answer.add(money / unit.get(i));
             money = money % unit.get(i);
