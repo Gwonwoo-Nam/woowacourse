57b3dbe4b1b6354dc08d77e4176dd37e97f40f5e
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 11:57:06 +0900

    feat : solution 기능 추가
    예외처리에 해당하는 check_List() 함수
    pobi와 crong의 페이지 계산 값을 반환하는 check_max 함수는 구현이 필요. 위 두 함수를 호출하여 비교 후 결과 값을 return함.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,19 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        int pobi_result;
+        int crong_result;
+
+        if (check_List(pobi, crong) == -1)
+            return -1; //예외처리
+        pobi_result = check_max(pobi);
+        crong_result = check_max(crong);
+        if (pobi_result > crong_result)
+            answer = 1;
+        else if (pobi_result == crong_result)
+            answer = 0;
+        else
+            answer = 2;
         return answer;
     }
 }
