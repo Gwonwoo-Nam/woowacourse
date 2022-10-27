45f97e5b812d7946195e72b4e908a5c0495d9aed
Gwonwoo Nam <ngw7617@naver.com>
2022-10-27 14:31:36 +0900

    Prob_4 exception : word length
    word의 길이 제한 : 1~1000이 아닐 시 "" return 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -4,6 +4,8 @@ public class Problem4 {
     public static String solution(String word) {
         String answer = "";
         int diff = 0;
+        if (word.length() < 1 || word.length() > 1000)
+            return "";
         for (int i = 0; i < word.length(); i++){
             if (word.charAt(i) >= 65 && word.charAt(i) <= 90) {
                 diff = word.charAt(i) - 65;
