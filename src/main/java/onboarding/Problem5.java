588fae802c4f7ecad28b727b4f8e95d43d42f81b
Gwonwoo-Nam <ngw7617@naver.com>
2022-10-28 10:41:40 +0900

    Prob_4 md : 각 문제 별 기능목록 markdown update

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -10,12 +10,18 @@ public class Problem5 {
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>();
         List<Integer> unit = new ArrayList<Integer>(List.of(50000, 10000, 5000, 1000, 500, 100, 50, 10, 1));
-        if (money < 1 || money > 1000000)
+        if (!check_money(money))
             return (Collections.emptyList());
         for(int i = 0; i < 9; i++){
             answer.add(money / unit.get(i));
             money = money % unit.get(i);
         }
         return answer;
+    }
+
+    private static boolean check_money(int money) {
+        if (money < 1 || money > 1000000)
+            return false;
+        return true;
     }
 }
