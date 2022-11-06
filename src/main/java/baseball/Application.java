e1548ea74e8d46de6205cf48799cf9728e0f26b0
Gwonwoo-Nam <ngw7617@naver.com>
2022-11-07 01:34:30 +0900

    feat : create empty user_answer class
    빈 클래스를 생성하였으며 사용자 입력을 읽어들이는 메서드인 read 메서드, 그리고 입력 받은 값을 저장하는 save 메서드로 구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+    public class user_answer{
+        private static void read(){
+
+        }
+
+        private static void save(){
+
+        }
+
+    }
+
     public static void main(String[] args) {
+
+
+
         // TODO: 프로그램 구현
     }
 }
