6cfabe9ab6ebb3344f26a36b9e924c9ed5f9ac76
Gwonwoo Nam <ngw7617@naver.com>
2022-11-13 12:24:25 +0900

    feat(lotto):create feature which can get purchaseAmount from user

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Integer;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
+        System.out.println("구입금액을 입력해 주세요.");
+        String ReadpurchaseAmount = Console.readLine();
+        int purchaseAmount = Integer.parseInt(ReadpurchaseAmount);
+
+        final int LOTTO_PRICE = 1000;
+        int lottoCount = purchaseAmount / LOTTO_PRICE;
+
+        System.out.printf("%d개를 구매했습니다.", lottoCount);
+
         // TODO: 프로그램 구현
     }
 }
