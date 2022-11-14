26b1d352c6635ee9b310e6c1118e4645aeac6e0d
Gwonwoo Nam <ngw7617@naver.com>
2022-11-14 18:47:19 +0900

    refactor(lotto):refactor private functions in play method
    - play 내부의 메서드를 기능 단위로 묶어 private method로 리팩터링

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -6,48 +6,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGame {
+    private static final int LOTTO_PRICE = 1000;
+    private static int lottoCount;
+    private static List<Integer> winningNumbers;
+
     public void play() {
-        //구매 금액 입력
         LottoManager lottoManager = new LottoManager();
         lottoManager.readPurchaseAmount();
-        //개수만큼 로또 생성
-        int lottoCount;
         lottoCount = lottoManager.getLottoCount();
+        //개수만큼 로또 생성
         Lotto[] lotto = new Lotto[lottoCount];
         generateLotto(lottoCount, lotto);
         //winning number 저장
-        List<Integer> winningNumbers = new ArrayList<>();
         lottoManager.readWinningNumbers();
         lottoManager.readBonusNumber();
         winningNumbers = lottoManager.getWinningNumbers();
-
         // Lotto 정답 비교
-        LottoRank[] values = LottoRank.values();
+        LottoRank[] rankBoard = getRankBoard(lotto);
+        // Lotto Rank 출력
+        showLottoRankBoard(rankBoard);
+        showLottoProfitResult(lottoCount, rankBoard);
+    }
+
+    private static LottoRank[] getRankBoard(Lotto[] lotto) {
+        LottoRank[] rankBoard = LottoRank.values();
         for (int currentCount = 0; currentCount < lottoCount; currentCount++) {
             WinningCounter winningCounter = new WinningCounter();
             winningCounter.countWinning(lotto[currentCount].getLottoNumbers(), winningNumbers);
-            int normalCount = winningCounter.getNormal_count();
-            int bonusCount = winningCounter.getBonus_count();
-
-            for (LottoRank rank : values) {
-                if (rank.checkThirdPlace(normalCount, bonusCount)) {
-                    break;
-                }
-                rank.findMatchingRank(normalCount);
-            }
-
+            updateRankBoard(rankBoard, winningCounter);
         }
-        // Lotto Rank 출력
+        return rankBoard;
+    }
+
+    private static void updateRankBoard(LottoRank[] rankBoard, WinningCounter winningCounter) {
+        int normalCount = winningCounter.getNormal_count();
+        int bonusCount = winningCounter.getBonus_count();
+        for (LottoRank rank : rankBoard) {
+            if (rank.checkThirdPlace(normalCount, bonusCount)) {
+                break;
+            }
+            rank.findMatchingRank(normalCount);
+        }
+    }
+
+    private static LottoPrinter showLottoRankBoard(LottoRank[] values) {
         LottoPrinter lottoPrinter = new LottoPrinter();
         lottoPrinter.printStatisticMessage();
         for (LottoRank rank : values) {
             lottoPrinter.printWinningResult(rank);
         }
+        return lottoPrinter;
+    }
+
+    private static void showLottoProfitResult(int lottoCount, LottoRank[] values) {
+        LottoPrinter lottoPrinter = new LottoPrinter();
         ProfitManager profitManager = new ProfitManager();
         for (LottoRank rank : values) {
             profitManager.sumEarning(rank);
         }
-        double totalProfit = profitManager.calculate(lottoManager.getPurchaseAmount());
+        int purchaseAmount = lottoCount * LOTTO_PRICE;
+        double totalProfit = profitManager.calculate(purchaseAmount);
         lottoPrinter.printProfit(totalProfit);
     }
 
