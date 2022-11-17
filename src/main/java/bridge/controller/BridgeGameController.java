3d4fb8303811428d13d4e4dcf049819714b96ced
Gwonwoo Nam <ngw7617@naver.com>
2022-11-17 22:40:55 +0900

    feat(bridge):create BridgeGameController class
    - game service를 control하는 controller 클래스 생성

--- src/main/java/bridge/controller/BridgeGameController.java
+++ src/main/java/bridge/controller/BridgeGameController.java
@@ -1,2 +1,35 @@
-package bridge.controller;public class BridgeGameController {
+package bridge.controller;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BridgeGameController {
+    public void run () {
+        OutputView outputView = new OutputView();
+        outputView.printStartMessage();
+        outputView.printInputBridgeLengthMessage();
+
+        InputView inputView = new InputView();
+        int bridgeSize = inputView.readBridgeSize();
+
+        BridgeRandomNumberGenerator bridgeRandomNumberGenerator = new BridgeRandomNumberGenerator();
+
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeRandomNumberGenerator);
+        List<String> bridge = new ArrayList<>();
+        bridge = bridgeMaker.makeBridge(bridgeSize);
+
+
+        BridgeGame bridgeGame = new BridgeGame(bridge);
+        for (int bridgeIndex = 0; bridgeIndex < bridgeSize; bridgeIndex++) {
+            String userBridgeType;
+            if (!bridgeGame.move(bridgeIndex, userBridgeType)) {
+                bridgeGame.retry();
+            }
+        }
+
+    }
+
 }
