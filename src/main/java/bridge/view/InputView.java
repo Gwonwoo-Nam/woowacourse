af69bd36b800622893f76ccfc77c5be9421e781d
Gwonwoo Nam <ngw7617@naver.com>
2022-11-17 15:51:28 +0900

    feat(view):create enum GameMessages holding message information
    - 게임 메시지를 저장한 enum 생성

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,4 +1,6 @@
-package bridge;
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
 
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
@@ -9,7 +11,10 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+
+        final int bridgeSize = Integer.parseInt(Console.readLine());
+
+        return bridgeSize;
     }
 
     /**
