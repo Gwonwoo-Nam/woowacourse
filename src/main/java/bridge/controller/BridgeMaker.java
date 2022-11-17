0a6c2cc3da5641d82a36d5c965ad4e777922dee3
Gwonwoo Nam <ngw7617@naver.com>
2022-11-17 22:07:08 +0900

    feat(bridge):create BridgeMaker class, makeBridge method
    - 다리 길이마다 이동 가능한 칸을 랜덤으로 저장하여 Bridge 배열로 리턴한다.
    - BridgeRandomNumberGenerator로부터 난수를 받아와 U/D를 결정한다.

--- src/main/java/bridge/controller/BridgeMaker.java
+++ src/main/java/bridge/controller/BridgeMaker.java
@@ -1,5 +1,7 @@
-package bridge;
+package bridge.controller;
 
+import bridge.BridgeNumberGenerator;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,6 +20,21 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        return null;
+        List<String> bridge = new ArrayList<String>();
+        int randomNumber;
+        for (int bridgeIndex = 0; bridgeIndex < size; bridgeIndex++) {
+            randomNumber = bridgeNumberGenerator.generate();
+            bridge.add(createBridgeShape(randomNumber));
+        }
+        return bridge;
     }
+
+    private String createBridgeShape(int randomNumber) {
+        final double HALF_POINT = 0.5;
+        if (randomNumber >= HALF_POINT) {
+            return "U";
+        }
+        return "D";
+    }
+
 }
