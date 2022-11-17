80405f91d5004e99cf19eae038650783d9054524
Gwonwoo Nam <ngw7617@naver.com>
2022-11-18 01:14:33 +0900

    feat(view):create printMap method in OutputView Class where it can print out map.
    - 게임 중간 결과 문구를 출력하는 printMap method 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,5 +1,7 @@
 package bridge.view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
@@ -15,12 +17,61 @@ public class OutputView {
         inputBridgeLengthMessage.print();
     }
 
+    public void printInputMoveMessage () {
+        GameMessages inputMoveMessage = GameMessages.INPUT_MOVE_MESSAGE;
+        inputMoveMessage.print();
+    }
+
+    public void printInputRetrialMessage () {
+        GameMessages inputRetrialMessage = GameMessages.INPUT_RETRIAL_MESSAGE;
+        inputRetrialMessage.print();
+    }
+
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(List<String> userBridge) {
+        printUpperBridge(userBridge);
+        printLowerBridge(userBridge);
+    }
+
+    private void printLowerBridge(List<String> userBridge) {
+        System.out.print("[ ");
+        for (String bridgeType : userBridge.subList(0, userBridge.size() - 1)) {
+            isDownBridge(bridgeType);
+            System.out.print(" | ");
+        }
+        isDownBridge(userBridge.get(userBridge.size() - 1));
+        System.out.println(" ]");
+    }
+
+    private void printUpperBridge(List<String> userBridge) {
+        System.out.print("[ ");
+        for (String bridgeType : userBridge.subList(0, userBridge.size() - 1)) {
+            isUpperBridge(bridgeType);
+            System.out.print(" | ");
+        }
+        isUpperBridge(userBridge.get(userBridge.size() - 1));
+        System.out.println(" ]");
+    }
+
+    private void isUpperBridge(String bridgeType) {
+        if (bridgeType.equals("U")) {
+            System.out.print("O");
+            return ;
+        }
+        System.out.print(" ");
+    }
+
+    private void isDownBridge(String bridgeType) {
+        if (bridgeType.equals("D")) {
+            System.out.print("O");
+            return ;
+        }
+        System.out.print(" ");
     }
 
     /**
