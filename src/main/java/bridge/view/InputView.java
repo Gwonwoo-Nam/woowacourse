10a73b304d52be7f776ac37f50d26d75dc2bfbd9
Gwonwoo Nam <ngw7617@naver.com>
2022-11-17 23:05:08 +0900

    feat(view):create readMoving method which can get user input to move
    - 라운드마다 플레이어가 이동할 칸을 입력 받는다.
    - U(위 칸)와 D(아래 칸) 중 하나의 문자를 입력할 수 있으며 올바른 값이 아니면 예외 처리한다.

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -10,16 +10,16 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() throws IllegalArgumentException {
+    public int readBridgeSize() {
         final int bridgeSize;
         final String input = Console.readLine();
-        validate(input);
+        validateBridgeSize(input);
         bridgeSize = Integer.parseInt(input);
 
         return bridgeSize;
     }
 
-    private void validate(String input) {
+    private void validateBridgeSize(String input) {
         final String numberRangeRegex = "^[3-9]{1}$|^[1]{1}[0-9]{1}$|^[2]{1}[0]{1}$";
         if (!input.matches(numberRangeRegex)) {
             throw new IllegalArgumentException("[ERROR] 3~20 범위의 숫자를 입력해주세요.");
@@ -30,8 +30,18 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        final String bridgeMoving = Console.readLine();
+        validateBridgeMoving(input);
+
+        return bridgeMoving;
     }
+    private void validateBridgeMoving(String input){
+        final String movingRegex = "^[UD]$";
+        if (!input.matches(movingRegex)) {
+            throw new IllegalArgumentException("[ERROR] U 혹은 D를 입력해주세요.");
+        }
+    }
+
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
