27e31d3e336f6c1fd59f762cc75a4b0936ff700c
Gwonwoo Nam <ngw7617@naver.com>
2022-11-19 02:14:32 +0900

    chore(view): change field names(messages)
    - 메시지 변수 명 가독성 개선 목적 업데이트

--- src/main/java/bridge/view/GameMessages.java
+++ src/main/java/bridge/view/GameMessages.java
@@ -6,7 +6,7 @@ public enum GameMessages {
     INPUT_MOVE_MESSAGE("이동할 칸을 선택해주세요. (위: U, 아래: D)"),
     INPUT_RETRIAL_MESSAGE("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"),
     END_MESSAGE("최종 게임 결과"),
-    IS_SUCCESS_MESSAGE("게임 성공 여부: "),
+    GAME_SUCCESS_MESSAGE("게임 성공 여부: "),
     RETRIAL_NUMBERS_MESSAGE("총 시도한 횟수: ");
 
     private String message;
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -92,12 +92,12 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult(int retrialNumber,boolean isSuccess, List<String> userBridge) {
+    public void printResult(int retrialNumber,boolean gameSuccess, List<String> userBridge) {
         printEndMessage();
         printMap(userBridge);
-        GameMessages isSuccessMessage = GameMessages.IS_SUCCESS_MESSAGE;
+        GameMessages isSuccessMessage = GameMessages.GAME_SUCCESS_MESSAGE;
         isSuccessMessage.print();
-        if (isSuccess == true) {
+        if (gameSuccess == true) {
             System.out.println("성공");
         }
         else {
