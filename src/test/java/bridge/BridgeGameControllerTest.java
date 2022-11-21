c841a9636bf9891dfd8630bb166dbdcf12d88836
Gwonwoo Nam <ngw7617@naver.com>
2022-11-21 17:19:11 +0900

    test(bridge):create generateBridge method test in BridgeGameController Class

--- src/test/java/bridge/BridgeGameControllerTest.java
+++ src/test/java/bridge/BridgeGameControllerTest.java
@@ -1,10 +1,14 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.util.Lists.newArrayList;
+
+import bridge.ApplicationTest.TestNumberGenerator;
 import bridge.controller.BridgeGameController;
-
+import bridge.model.BridgeMaker;
 import java.io.ByteArrayInputStream;
+import java.util.List;
 import org.assertj.core.api.SoftAssertions;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,5 +32,31 @@ class BridgeGameControllerTest {
             softAssertions.assertThat(bridgeGameController.decideBridgeLength()).isEqualTo(7);
             softAssertions.assertAll();
         }
+    }
+
+    @DisplayName("generateBridge 메소드는")
+    @Nested
+    class Describe_generateBridge {
+        @DisplayName("다리의 길이를 입력받아 다리를 생성한다.")
+        @Test
+        void generateBridgeTest () {
+            TestBridgeGameController testBridgeGameController = new TestBridgeGameController();
+            List<String> bridge = testBridgeGameController.generateBridge(7);
+
+            assertThat(bridge).isEqualTo(List.of("U","D","U","D","U","D","U"));
+        }
+
+    }
+    class TestBridgeGameController extends BridgeGameController {
+        @Override
+        public List<String> generateBridge(int bridgeSize) {
+            TestNumberGenerator testNumberGenerator = new TestNumberGenerator(newArrayList(1,0,1,0,1,0,1));
+            BridgeMaker bridgeMaker = new BridgeMaker(testNumberGenerator);
+
+            final List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+
+            return bridge;
+        }
+
     }
 }
--- src/test/java/bridge/BridgeGameProcessorTest.java
+++ src/test/java/bridge/BridgeGameProcessorTest.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import bridge.controller.BridgeGameProcessor;
 import bridge.model.BridgeGame;
 import java.io.ByteArrayInputStream;
@@ -7,7 +9,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
-
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -21,17 +22,20 @@ class BridgeGameProcessorTest {
     @DisplayName("chooseDirection 메소드는")
     @Nested
     class Describe_chooseDirection {
+
         SoftAssertions softAssertions = new SoftAssertions();
+
         @DisplayName("방향을 입력받는다.")
         @Test
-        void chooseDirectionTest () {
+        void chooseDirectionTest() {
             BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "U"));
             BridgeGameProcessor bridgeGameProcessor = new BridgeGameProcessor(bridgeGame);
             System.setIn(new ByteArrayInputStream("U".getBytes()));
             OutputStream out = new ByteArrayOutputStream();
             System.setOut(new PrintStream(out));
 
-            softAssertions.assertThat(bridgeGameProcessor.chooseDirection()).as("입력 확인").isEqualTo("U");
+            softAssertions.assertThat(bridgeGameProcessor.chooseDirection()).as("입력 확인")
+                    .isEqualTo("U");
             softAssertions.assertThat(out.toString()).as("출력 확인").contains("이동할 칸을 선택해주세요.");
             softAssertions.assertAll();
         }
@@ -44,17 +48,19 @@ class BridgeGameProcessorTest {
         @DisplayName("건너려는 타일이 일치하면")
         @Nested
         class Context_with_correctMove {
+
             SoftAssertions softAssertions = new SoftAssertions();
             BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "U"));
             BridgeGameProcessor bridgeGameProcessor = new BridgeGameProcessor(bridgeGame);
+
             @DisplayName("다리를 한 칸 건너고 배열에 업데이트 한다.")
             @Test
-            void makeMoveTest () {
+            void makeMoveTest() {
                 OutputStream out = new ByteArrayOutputStream();
                 System.setOut(new PrintStream(out));
                 bridgeGameProcessor.makeMove("U");
                 System.setIn(new ByteArrayInputStream("R".getBytes()));
-                softAssertions.assertThat(out.toString()).as("메시지 확인").contains("[ O ]","[   ]");
+                softAssertions.assertThat(out.toString()).as("메시지 확인").contains("[ O ]", "[   ]");
                 int upSideIndex = out.toString().indexOf("[ O ]");
                 int downSideIndex = out.toString().indexOf("[   ]");
                 softAssertions.assertThat(upSideIndex).as("인덱스 위치 확인").isLessThan(downSideIndex);
@@ -65,4 +71,32 @@ class BridgeGameProcessorTest {
 
     }
 
+    @DisplayName("crossBridge 메소드는")
+    @Nested
+    class Describe_crossBridge {
+
+        BridgeGame bridgeGame = new BridgeGame(List.of("U", "D", "U"));
+        BridgeGameProcessor bridgeGameProcessor = new BridgeGameProcessor(bridgeGame);
+
+        @DisplayName("다리가 완성될 때까지 다리를 한 칸 씩 건넌다.")
+        @Test
+        void makeMoveTest() {
+            OutputStream out = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(out));
+
+            String input = "D\nR\nU\nD\nU";
+            System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+            bridgeGameProcessor.crossBridge();
+            assertThat(out.toString())
+                    .contains("[ X ]",
+                            "[ O ]",
+                            "[ O |   ]",
+                            "[ O |   | O ]",
+                            "[   | O |   ]",
+                            "게임을 다시 시도");
+        }
+    }
 }
+
+
