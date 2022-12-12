8b27490e8ef87cd49e53a4e929abe80a3497305d
Gwonwoo-Nam <ngw7617@naver.com>
2022-12-13 04:22:52 +0900

    refactor(racingcar):CarGameController 클래스 Car 이름과 시도 횟수를 정하는 메서드 리팩터링

--- src/main/java/racingcar/Controller/CarGameController.java
+++ src/main/java/racingcar/Controller/CarGameController.java
@@ -3,48 +3,69 @@ package racingcar.Controller;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import racingcar.Car;
+import racingcar.ErrorMessages;
+import racingcar.Model.Car;
+import racingcar.Model.Cars;
+import racingcar.Regex;
 import racingcar.View.InputView;
 import racingcar.View.OutputView;
 
 public class CarGameController {
 
     public void play() {
-        OutputView outputView = new OutputView();
-        outputView.askCarName();
+        Cars cars = new Cars();
 
-        InputView inputView = new InputView();
-        List<String> carNameList = new ArrayList<>(inputView.readCarList());
+        setCarsName(cars);
 
-        outputView.askTrialNumber();
+        setTrialNumber(cars);
 
-        int trialNumber = inputView.readTrialNumber();
+        OutputView.printResultMessage();
 
-        outputView.printResultMessage();
-        List<Car> carList = new ArrayList<>();
-        for(String carName : carNameList) {
-            Car car = new Car(carName);
-            carList.add(car);
+        for (int number = 0; number < cars.getTrialNumber(); number++) {
+            driveCars(cars.getCars().getCarList());
         }
+    }
 
-        for (int number = 0; number < trialNumber; number++) {
-            driveCars(carList);
+    private void setCarsName(Cars cars) {
+        try {
+            cars.getCars().clear();
+            cars.addCar(InputView.readCarsName());
+            cars.validate();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setCarsName(cars);
+        }
+    }
+
+    private void setTrialNumber(Cars cars) {
+
+        try {
+            String input = InputView.readTrialNumber();
+            validateTrialNumber(input);
+            cars.setTrialNumber(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            setTrialNumber(cars);
+        }
+    }
+
+    public void validateTrialNumber(String input) {
+        if (!input.matches(Regex.INPUT_TRIAL_NUMBER_REGEX.print())) {
+            throw new IllegalArgumentException(ErrorMessages.TRIAL_NUMBER_ERROR_MESSAGE.print());
         }
     }
 
     public void driveCars(List<Car> carList) {
-        OutputView outputView = new OutputView();
-
         for (Car car : carList) {
             int randomNumber = pickRandomNumber();
             car.move(randomNumber);
-            outputView.printResult(car.getName(), car.getPosition());
+            OutputView.printResult(car.getName(), car.getPosition());
         }
         System.out.println();
     }
 
     public int pickRandomNumber() {
-        int randomNumber = Randoms.pickNumberInRange(0,9);
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
 
         return randomNumber;
     }
