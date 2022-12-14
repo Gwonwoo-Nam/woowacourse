53b6f8a1f341d2fe23334e3fd1dcc12c78f472ed
Gwonwoo-Nam <ngw7617@naver.com>
2022-12-15 08:12:19 +0900

    feat(subway-map):InfoMessages, RequestMessages에서 메시지를 저장하는 enum 생성

--- src/main/java/subway/InfoMessages.java
+++ src/main/java/subway/InfoMessages.java
@@ -1,2 +1,41 @@
-package subway;public enum InfoMessages {
+package subway;
+
+public enum InfoMessages {
+    MAIN("## 메인 화면\n"
+        + "1. 역 관리\n"
+        + "2. 노선 관리\n"
+        + "3. 구간 관리\n"
+        + "4. 지하철 노선도 출력\n"
+        + "Q. 종료"),
+    STATION_MANAGEMENT("1. 역 등록\n"
+        + "2. 역 삭제\n"
+        + "3. 역 조회\n"
+        + "B. 돌아가기\n"),
+    LINE_MANAGEMENT("## 노선 관리 화면\n"
+        + "1. 노선 등록\n"
+        + "2. 노선 삭제\n"
+        + "3. 노선 조회\n"
+        + "B. 돌아가기"),
+    SECTION_MANAGEMENT("## 구간 관리 화면\n"
+        + "1. 구간 등록\n"
+        + "2. 구간 삭제\n"
+        + "B. 돌아가기"),
+
+    STATION_REGISTRATION("[INFO] 지하철 노선이 등록되었습니다."),
+    STATION_DELETION("[INFO] 지하철 노선이 삭제되었습니다."),
+    SECTION_REGISTRATION("[INFO] 구간이 등록되었습니다."),
+    SECTION_DELETION("[INFO] 구간이 삭제되었습니다.");
+
+
+
+
+    private String Message;
+    InfoMessages(String Message) {
+        this.Message = Message;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
 }
--- src/main/java/subway/RequestMessages.java
+++ src/main/java/subway/RequestMessages.java
@@ -1,2 +1,27 @@
-package subway;public enum RequestMessages {
+package subway;
+
+public enum RequestMessages {
+    FEATURE("## 원하는 기능을 선택하세요."),
+    REGISTRATION_STATION("## 등록할 역 이름을 입력하세요."),
+    DELETION_STATION("## 삭제할 역 이름을 입력하세요."),
+
+    REGISTRATION_LINE("## 등록할 노선 이름을 입력하세요."),
+    UPSTREAM_ENDPOINT("## 등록할 노선의 상행 종점역 이름을 입력하세요."),
+    DOWNSTREAM_ENDPOINT("## 등록할 노선의 하행 종점역 이름을 입력하세요."),
+    DELETION_LINE("## 삭제할 노선 이름을 입력하세요."),
+
+    SECTION_REGISTRATION_LINE("## 노선을 입력하세요."),
+    SECTION_REGISTRATION_STATION("## 역이름을 입력하세요."),
+    SECTION_REGISTRATION_ORDER("## 순서를 입력하세요."),
+    SECTION_DELETION_LINE("## 삭제할 구간의 노선을 입력하세요."),
+    SECTION_DELETION_STATION("## 삭제할 구간의 역을 입력하세요.");
+
+    private String Message;
+    RequestMessages(String Message) {
+        this.Message = Message;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
 }
